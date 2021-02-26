2d1094dde237be564c5fe1ba81a38274235efb7d
Ilya Usov <iusov41@gmail.com>
2021-02-26 16:34:44 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(num) {
+    if (num == 0) {
+        return 'zero';
+    }
+    let arr = [0, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+        arr2 = ['and', 0, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+        arr3 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+        hundreds = Math.floor(num / 100),
+        tens = Math.floor((num / 10) % 10),
+        ones = (num % 100) % 10,
+        result = '';
+
+    if (hundreds != 0) {
+        result += (arr[hundreds] + ' hundred');
+    }
+    if((ones!=0 || tens !=0) && hundreds!=0){
+        result+= ' ';
+    }
+    if (tens == 1) {
+        result += arr3[ones];
+        return result;
+    }
+    if (tens != 0) {
+        result += arr2[tens];
+    }
+    if(ones!=0){
+        result+= ' ';
+    }
+    if (ones != 0) {
+        result += arr[ones];
+    }
+    result = result.replace(/\s+/g, ' ').trim();
+    return result;
 }
